86e2b5231d9304b01d8ff767a7e81c732e7fd07a
Claudius26 <osomzed@gmail.com>
2025-06-14 12:37:44 +0100

    commit

---
+++ ecommerce_project/store/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'price','available', 'created_at', 'updated_at' )
+    list_filter = ('available', 'created_at', 'updated_at', 'category')
+    search_fields = ('name', 'description')
---
+++ ecommerce_project/store/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+# Create your models here.
